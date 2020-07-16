7839dc734e799611e14c5c2e2048874f6f28eddb
Khemmachat Thongkhum <khemmachat.tho@ascendcorp.com>
2020-07-16 15:09:39 +0700

    Initial Commit

---
+++ Eggcial/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Eggcial
+//
+//  Created by Khemmachat Thongkhum on 16/7/2563 BE.
+//  Copyright © 2563 Khemmachat Thongkhum. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
